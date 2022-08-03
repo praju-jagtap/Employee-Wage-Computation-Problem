03df1866bc1546756c1c1d45b439aab85a7fc6a1
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-03 15:34:48 +0530

    Added Part time employee and wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo ""
+echo "Welcome to Employee Wage Computation Program"
+echo ""
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [[ $isPresent -eq $randomCheck ]]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
 is_Full_Time=1
 emp_Rate_Per_Hour=20
 randomCheck=$(( RANDOM%2 ))
@@ -8,4 +21,26 @@ then
         emphrs=8
 fi
         salary=$(($emphrs*$emp_Rate_Per_Hour))
+        echo $salary
+
+isPartTime=2
+isFullTime=1
+emp_Rate_Per_Hour=20
+randomCheck=$((( RANDOM%3 ) +1))
+
+case $randomCheck in
+
+        $isFullTime)
+                emphrs=8
+                ;;
+
+        $isPartTime)
+                emphrs=4
+                ;;
+        *)
+                emphrs=0
+                ;;
+esac
+
+        salary=$(( $emphrs * $emp_Rate_Per_Hour ))
         echo $salary
