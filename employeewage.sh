3ff1539a51622c92ed0885df645e12c477bea402
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-03 15:44:38 +0530

    Calculating Wages

--- employeewage.sh
+++ employeewage.sh
@@ -76,3 +76,34 @@ esac
 done
         echo "Total Salary = $totalSalary"
 
+
+isPartTime=2
+isFullTime=1
+emp_Rate_Per_Hour=20
+totalSalary=0
+no_Of_Working_Days=20
+
+
+for (( day=1; day<=$no_Of_Working_Days; day++ ))
+do
+        randomCheck=$((( RANDOM%3 ) +1))
+
+case $randomCheck in
+
+        $isFullTime)
+                emphrs=8
+                ;;
+
+        $isPartTime)
+                emphrs=4
+                ;;
+        *)
+                emphrs=0
+                ;;
+esac
+
+        salary=$(( $emphrs * $emp_Rate_Per_Hour ))
+        echo $salary
+        totalSalary=$(( $totalSalary + $salary ))
+done
+        echo "Total Salary = $totalSalary"
